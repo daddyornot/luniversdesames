af37625bca482f4d6127dac7df0515377efe7287
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2026-01-30 16:37:13 +0100

    fix url

--- admin/src/app/app.config.ts
+++ admin/src/app/app.config.ts
@@ -1,14 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './core/interceptors/auth.interceptor';
+import {ApiModule, Configuration} from './core/api';
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    importProvidersFrom(
+      ApiModule.forRoot(() => {
+        return new Configuration({
+          basePath: environment.apiUrl // Utilise '/api' (dev) ou l'URL complète (prod)
+        });
+      })
+    )
   ]
 };
