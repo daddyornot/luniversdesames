f1afd522098b410498017af0b7cea4f0c3efa02e
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2026-01-30 17:02:11 +0100

    fix url

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,9 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import {provideRouter, withComponentInputBinding, withInMemoryScrolling, withViewTransitions} from '@angular/router';
 import {provideClientHydration} from '@angular/platform-browser';
 
@@ -6,6 +11,8 @@ import {routes} from './app.routes';
 import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './core/interceptors/auth.interceptor';
 import {provideNgxStripe} from 'ngx-stripe';
+import {ApiModule, Configuration} from './core/api';
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,6 +30,13 @@ export const appConfig: ApplicationConfig = {
     ),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     // Remplacez par votre clé publique Stripe de test
-    provideNgxStripe('pk_test_51Ss298HFTqvl1QymK8LgVzlsKNBPFlBmFSKTfoWVYeFsDfjMvUOugQKZ3OSw7qd0KvSB2BbYL6OoljioY5bls57W00lYT7csOE')
+    provideNgxStripe('pk_test_51Ss298HFTqvl1QymK8LgVzlsKNBPFlBmFSKTfoWVYeFsDfjMvUOugQKZ3OSw7qd0KvSB2BbYL6OoljioY5bls57W00lYT7csOE'),
+    importProvidersFrom(
+      ApiModule.forRoot(() => {
+        return new Configuration({
+          basePath: environment.apiUrl // Utilise '/api' (dev) ou l'URL complète (prod)
+        });
+      })
+    )
   ]
 };
